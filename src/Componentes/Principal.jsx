a8b509452d5dbe048074bef54c8c979d8cdf2d0f
Gustavo Silva <gustavo.femar@marinha.mil.br>
2024-09-05 14:24:08 -0300

    ajuste responsividade

--- src/Componentes/Principal.jsx
+++ src/Componentes/Principal.jsx
@@ -13,4 +13,8 @@ const Div = styled.div`
   background: url("modelo.svg") no-repeat center;
   background-size: 500px 500px;
   background-color: ${AzulFundo};
+
+  @media screen and (max-width: 600px) {
+    background-size: 300px 300px;
+  }
 `;
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,9 +3,18 @@ import "./globals.css";
 
 export default function RootLayout({children}) {
   return (
-    <html lang="pt-br">
-      <title>{`<Systech Dev />`}</title>
-      <body>{children}</body>
-    </html>
+    <>
+      <html lang="pt-br">
+        <head>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>{`<Systech Dev />`}</title>
+        </head>
+        <body>{children}</body>
+      </html>
+    </>
   );
 }
